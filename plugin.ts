7eab03dbb3f8123910fceba197245fad6cda1b4a
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-04-26 08:16:36 +0200

    fix compare

--- plugin.ts
+++ plugin.ts
@@ -1,93 +1,63 @@
 const app = ui.createProjectPanelSection();
-const panel = new ui.Panel("Vergleichen");
 
-// section for active varaint information
-const section = new ui.Section('aktive Variante');
-app.add(section);
-app.add(new ui.Button('Varianten Vergleichen', () => panel.open()))
+// create panel
+const panel = new ui.Panel('Vergleichen');
 
-const actualVariantArea = new ui.LabeledValue('Fläche Total', '-  m²');
-const actualVariantVolume = new ui.LabeledValue('Volume Total', '- m³');
-section.add(actualVariantArea);
-section.add(actualVariantVolume);
+data.onProjectSelect.subscribe(async project => {
+    app.removeAllChildren();
 
-// content of variant comparsion 
-const barChartSectionVolume = new ui.Container();
-const barChartSectionArea = new ui.Container();
-// app.add(new ui.Separator());
-panel.add(barChartSectionVolume);
-panel.add(new ui.Separator());
-panel.add(barChartSectionArea);
+    // section for active varaint information
+    const section = new ui.Section('Aktive Variante');
+    app.add(section);
 
-const createVariantBarChart = (name: string, values: any) => {
+    const areaLabel = new ui.LabeledValue('Fläche Total', '- m²');
+    section.add(areaLabel);
 
-    const barChart = new ui.BarChart(name, 'm³');
-    for (let item of values) {
-        barChart.addSegment(item.name, Math.round(item.value));
-    }
-    return barChart;
-} 
+    const volumeLabel = new ui.LabeledValue('Volume Total', '- m³');
+    section.add(volumeLabel);
 
-data.onProjectSelect.subscribe(project => {
     if (project) {
-
         // get informations of active variant 
         project.onVariantSelect.subscribe(variant => {
             if (variant) {
                 section.name = variant.name;
-                actualVariantArea.value = Math.round(variant.floorArea).toLocaleString('de-CH')  + ' m²';
-                actualVariantVolume.value = Math.round(variant.volume).toLocaleString('de-CH') + ' m³';
+
+                variant.onFloorAreaChange.subscribe(area => areaLabel.value = area.toMetricAreaString());
+                variant.onVolumeChange.subscribe(volume => volumeLabel.value = volume.toMetricVolumeString());
             } else {
-                section.name = 'keine aktive Variante';
-                actualVariantArea.value = '-  m²';
-                actualVariantVolume.value = '- m³';
+                section.name = 'Keine Variante Ausgewählt';
+
+                areaLabel.value = '- m²';
+                volumeLabel.value = '- m³';
             }
-        })
-
-
-        // get informations of all variants
-        project.getVariants().then(projectVars => {
-            barChartSectionVolume.removeAllChildren();
-            barChartSectionArea.removeAllChildren();
-
-            // get max values of floor and volume for charting
-            let _maxVolume = 0;
-            let _maxFloorArea = 0;
-            for (let varData of projectVars) {
-                if (varData.volume > _maxVolume) {
-                    _maxVolume = varData.volume;
-                } 
-                if (varData.floorArea > _maxFloorArea) {
-                    _maxFloorArea = varData.floorArea;
-                }
-            }
-            const maxVolume = _maxVolume;
-            const maxFloorArea = _maxFloorArea;
-
-            // list all variant volumes 
-            barChartSectionVolume.add(new ui.Paragraph('Variantenvergleich Volumen'))
-            for (let varData of projectVars) {
-                // bar chart data
-                const varVol = [{name:'Total Volume', value:varData.volume}];
-
-                // create labeled values instead of bar chart
-                barChartSectionVolume.add(new ui.LabeledValue(varData.name, Math.round(varData.volume).toLocaleString('de-CH') + ' m³'))
-                // barChartSectionVolume.add(createVariantBarChart(varData.name, varVol));
-            }
-
-            // list all variant floor areas 
-            barChartSectionArea.add(new ui.Paragraph('Variantenvergleich Fläche'))
-            for (let varData of projectVars) {
-                // bar chart data
-                const varVol = [{name:'Total Volume', value:varData.floorArea}];
-
-                // create labeled values instead of bar chart
-                barChartSectionArea.add(new ui.LabeledValue(varData.name, Math.round(varData.floorArea).toLocaleString('de-CH') + ' m²'))
-                // barChartSectionArea.add(createVariantBarChart(varData.name, varVol));
-            }
-
         });
+
+        section.add(new ui.Button('Varianten Vergleichen', async () => {
+            panel.removeAllChildren();
+        
+            const variants = await data.selectedProject.getVariants();
+
+            const maxArea = Math.max(...variants.map(variant => variant.floorArea));
+            const maxVolume = Math.max(...variants.map(volume => volume.volume));
+
+            for (let variant of variants) {
+                const section = new ui.Section(variant.name);
+                panel.add(section);
+
+                const areaChart = new ui.BarChart('Area', 'm²');
+                areaChart.addSegment(variant.name, variant.floorArea);
+                areaChart.max = maxArea;
+
+                section.add(areaChart);
+
+                const volumeChart = new ui.BarChart('Volume', 'm³');
+                volumeChart.addSegment(variant.name, variant.volume);
+                volumeChart.max = maxVolume;
+
+                section.add(volumeChart);
+            }
+        
+            panel.open();
+        }));
     }
-})
-
-
+});
