c48104beaf14b7b35ec99a7ea50f22b435577607
gitlab_pipeline <gitlab_pipeline@luucy.ch>
2025-08-01 00:20:22 +0000

    Crowdin update on 20250801-0020

--- i18n/building-metrics.fr.ts
+++ i18n/building-metrics.fr.ts
@@ -8,7 +8,7 @@ export default {
   Metrics: "Métriques",
   Usages: "Utilisation",
   No_variant_selected: "Aucune variante sélectionnée",
-  No_building: 'Aucun bâtiment dans la variante sélectionnée',
+  No_building: 'No buildings in selected variant',
   Selected_Variant: "Variante sélectionnée",
   Export_to_Excel: "Exportation vers Excel (CSV)",
   Formatted: "Formaté",
@@ -16,5 +16,5 @@ export default {
   Download_CSV: "Télécharger le CSV",
   Footprint: "Empreinte",
   Loading: "Chargement",
-  Not_enough_variants: "Pas assez de variantes",
+  Not_enough_variants: "Pas assez de variantes"
 };
